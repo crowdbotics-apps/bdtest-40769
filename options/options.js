7fa5300ef6606a2adf7a75419c315ea4d78fd347
Bryan Dunn <bryan.dunn@crowdbotics.com>
2023-05-18 12:14:23 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "bdtest-40769",
-  url: "https://bdtest-40769.botics.co",
-  api: "https://bdtest-40769.botics.co/api/v1"
-}
+  "name": "bdtest-40769",
+  "url": "https://bdtest-40769.botics.co",
+  "api": "https://bdtest-40769.botics.co/api/v1",
+  "initialRoute": "login"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
